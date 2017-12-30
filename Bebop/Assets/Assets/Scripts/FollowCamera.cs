83b07ee077cb07200c87a927ab08ccddc43b0cc6
Lurkomorie <fomorden@gmail.com>
2017-12-30 20:32:44 +0200

    Too many changes to say

---
+++ Bebop/Assets/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public float xOffset = 0f;
+
+    public float yOffset = 0f;
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        this.transform.position = new Vector3(player.transform.position.x + xOffset, this.transform.position.y + yOffset, -10);
+    }
+
+}
